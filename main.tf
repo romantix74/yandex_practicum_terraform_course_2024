41f5b3cba1a85100c954335b4328b9d5ddf1776c
Ro <romantix74@yandex.ru>
2024-07-12 11:13:28 +0300

    local + outputs

--- main.tf
+++ main.tf
@@ -1,20 +1,32 @@
+# Local values
+locals {
+  boot_disk_name      = "${var.name_prefix}-boot-disk"
+  linux_vm_name       = "${var.name_prefix}-linux-vm"
+  vpc_network_name    = "${var.name_prefix}-private"
+  ydb_serverless_name = "${var.name_prefix}-ydb-serverless"
+  bucket_sa_name      = "${var.name_prefix}-bucket-sa"
+  bucket_name         = "${var.name_prefix}-terraform-bucket-${random_string.bucket_name.result}"
+}
+
 # Создание диска и виртуальной машины
 resource "yandex_compute_disk" "boot_disk" {
-  name     = "boot-disk"
-  zone     = "ru-central1-a"
-  image_id = "fd8ba9d5mfvlncknt2kd" # Ubuntu 22.04 LTS
-  size     = 15
+  name     = local.boot_disk_name
+  zone     = var.zone
+  image_id = var.image_id
+  
+  type = var.instance_resources.disk.disk_type
+  size = var.instance_resources.disk.disk_size
 }
 
 resource "yandex_compute_instance" "this" {
-  name                      = "linux-vm"
+  name                      = local.linux_vm_name
   allow_stopping_for_update = true
-  platform_id               = "standard-v3"
-  zone                      = "ru-central1-a"
+  platform_id               = var.instance_resources.platform_id
+  zone                      = var.zone
 
   resources {
-    cores  = "4"
-    memory = "8"
+    cores  = var.instance_resources.cores
+    memory = var.instance_resources.memory
   }
 
   boot_disk {
@@ -28,29 +40,29 @@ resource "yandex_compute_instance" "this" {
 
 # Создание VPC и подсети
 resource "yandex_vpc_network" "this" {
-  name = "private"
+  name = local.vpc_network_name
 }
 
 resource "yandex_vpc_subnet" "private" {
-  name           = "private"
-  zone           = "ru-central1-a"
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  name           = keys(var.subnets)[0]
+  zone           = var.zone
+  v4_cidr_blocks = var.subnets[keys(var.subnets)[0]]
   network_id     = yandex_vpc_network.this.id
 }
 
 # Создание Yandex Managed Service for YDB
 resource "yandex_ydb_database_serverless" "this" {
-  name = "test-ydb-serverless"
+  name = local.ydb_serverless_name
 }
 
 # Создание сервисного аккаунта 
 resource "yandex_iam_service_account" "bucket" {
-  name = "bucket-sa"
+  name = local.bucket_sa_name
 }
 
 # Назначение роли сервисному аккаунту
 resource "yandex_resourcemanager_folder_iam_member" "storage_editor" {
-  folder_id = "b1gfe1trpem7cgvho5uk"
+  folder_id = var.folder_id
   role      = "storage.editor"
   member    = "serviceAccount:${yandex_iam_service_account.bucket.id}"
 }
@@ -63,7 +75,7 @@ resource "yandex_iam_service_account_static_access_key" "this" {
 
 # Создание бакета 
 resource "yandex_storage_bucket" "this" {
-  bucket     = "terraform-bucket-${random_string.bucket_name.result}"
+  bucket     = local.bucket_name
   access_key = yandex_iam_service_account_static_access_key.this.access_key
   secret_key = yandex_iam_service_account_static_access_key.this.secret_key
   
@@ -75,6 +87,3 @@ resource "random_string" "bucket_name" {
   special = false
   upper   = false
 }
-
-
-
---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "boot_disk_id" {
+  description = "The ID of the boot disk created for the instance."
+  value       = yandex_compute_disk.boot_disk.id
+}
+
+output "instance_id" {
+  description = "The ID of the Yandex Compute instance."
+  value       = yandex_compute_instance.this.id
+}
+
+output "subnet_id" {
+  description = "The ID of the VPC subnet used by the Yandex Compute instance."
+  value       = yandex_vpc_subnet.private.id
+}
+
+output "ydb_id" {
+  description = "The ID of the Yandex Managed Service for YDB instance."
+  value       = yandex_ydb_database_serverless.this.id
+}
+
+output "service_account_id" {
+  description = "The ID of the Yandex IAM service account."
+  value       = yandex_iam_service_account.bucket.id
+}
+
+output "bucket_name" {
+  description = "The name of the Yandex Object Storage bucket."
+  value       = yandex_storage_bucket.this.bucket
+}
